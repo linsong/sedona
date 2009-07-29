f833beeca58b12219b241633dd6497a99eca7306
Brian Frank <brian@tridium.com>
2009-07-29 12:47:36 -0400

    docs: remove certification as top level tab

--- src/sedonac/src/sedonac/steps/WriteDoc.java
+++ src/sedonac/src/sedonac/steps/WriteDoc.java
@@ -127,7 +127,6 @@ public class WriteDoc
       out.w("  <li><a href='").w(home).w("'>Home</a></li>\n");
       out.w("  <li><a class='active' href='index.html'>Documentation</a></li>\n");
       out.w("  <li><a href='../../community.html'>Community</a></li>\n");
-      out.w("  <li><a href='../../certification.html'>Certification</a></li>\n");
       out.w("  <li><a href='/download/'>Download</a></li>\n");
       out.w("  <li><a href='http://groups.google.com/group/sedonadev'>Forum</a></li>\n");
       out.w("</ul>\n");    
