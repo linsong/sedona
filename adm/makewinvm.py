8adc25cdd602046078e59e6938a46d98279ec244
Elizabeth McKenney <emckenney@tridium.com>
2012-02-07 08:56:09 -0500

    makewinvm: add args for ipv4 & ipv6

--- adm/makewinvm.py
+++ adm/makewinvm.py
@@ -10,6 +10,7 @@
 
 import os
 import env
+import argparse
 import fileutil
 import compilewin
 import compilekit
@@ -30,12 +31,23 @@ libs = [ "ws2_32.lib",  "uuid.lib", "kernel32.lib"]
 defs = []
 
 
-# Make
-def compile():
+# initParser
+def initParser():
+  global parser
+  parser = argparse.ArgumentParser(description='Make Sedona VM for Windows')
+  ipgroup = parser.add_mutually_exclusive_group()
+  ipgroup.add_argument('-4', '--ipv4', action='store_true', default=False,  
+                             help='Use IPv4 protocol (default)')
+  ipgroup.add_argument('-6', '--ipv6', action='store_true', default=False, 
+                             help='Use IPv6 protocol')
+
+
+# compile
+def compile(cdefs=defs):
   try:
     fileutil.rmdir(stageDir)
     compilekit.compile(platFile + " -outDir " + stageDir)
-    compilewin.compile(exeFile, srcFiles, includes, libs, defs)
+    compilewin.compile(exeFile, srcFiles, includes, libs, cdefs)
 
   except env.BuildError:
     print "**"
@@ -45,6 +57,28 @@ def compile():
 
 # Main
 if __name__ == '__main__':
-  compile()
+  global parser
+  config = []
+
+  # Parse command line arguments
+  initParser()
+  options = parser.parse_args()
+
+  # Add command line arg to select ipv4 vs. ipv6 socket family
+  if (options.ipv4):
+    config = [("SOCKET_FAMILY_INET","0")]   # value doesn't matter
+    print " Building Sedona VM using IPv4 protocol.\n"
+  elif (options.ipv6):
+    config = [("SOCKET_FAMILY_INET6","0")]   # value doesn't matter
+    print " Building Sedona VM using IPv6 protocol.\n"
+  #else:
+  #  print " Must specify --ipv4 or --ipv6 option.\n"
+  #  exit(-1)
+
+  # Compile Sedona VM
+  compile(config)
+
+  # Create platform archive and install in platform DB
   platArchive.main(["--db", "--stage", os.path.join(stageDir, ".par")])
+
 
