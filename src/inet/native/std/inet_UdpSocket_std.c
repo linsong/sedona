06ece4f2670a7bba9145e057c485993539038d2b
Elizabeth McKenney <emckenney@tridium.com>
2012-06-18 15:50:46 -0400

    inet: remove unused native code (svm doesn't support both IPv4 & IPv6 simultaneously)

--- src/inet/native/std/inet_UdpSocket_std.c
+++ src/inet/native/std/inet_UdpSocket_std.c
@@ -194,19 +194,14 @@ Cell inet_UdpSocket_join(SedonaVM* vm, Cell* params)
   // Join all-hosts multicast address group (used for device discovery)
   //
 
-#if defined( SOCKET_FAMILY_INET6 )
-  if (strchr(addr, ':')==NULL)          // assume IPv4 if addr contains no ':' chars
-#endif
-
+#if defined( SOCKET_FAMILY_INET )
   {
     struct ip_mreq mreq;
     mreq.imr_multiaddr.s_addr = inet_addr(addr);
     mreq.imr_interface.s_addr = INADDR_ANY;
     rc = setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq));
   }
-
-#if defined( SOCKET_FAMILY_INET6 )
-  else
+#elif defined( SOCKET_FAMILY_INET6 )
   {
     struct ipv6_mreq mreq;
     inet_pton( AF_INET6, addr, &(mreq.ipv6mr_multiaddr) );
