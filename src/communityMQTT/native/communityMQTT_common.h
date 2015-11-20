3abd8e8cb30d6d0869eae50539f8b2eabf35571d
Vincent Wang <vincent@linsong.org>
2015-11-20 22:20:46 +0800

    refactoring && fix bugs

--- src/communityMQTT/native/communityMQTT_common.h
+++ src/communityMQTT/native/communityMQTT_common.h
@@ -7,6 +7,11 @@ typedef struct
 {
   Network * pNetwork;
   Client * pClient;
+  char * buf;
+  int buf_len;
+  char * readbuf;
+  int readbuf_len;
+  unsigned int command_timeout_ms;
 } MQTTHandle;
 
 #endif /* __COMMON_H__ */
